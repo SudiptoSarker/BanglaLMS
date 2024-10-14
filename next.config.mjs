87751e27490e9edb9417e32826d17db05ed772d7
adnanjabait <adnan.jabait@gmail.com>
2024-10-14 19:54:25 +0600

    Build error solved

--- next.config.mjs
+++ next.config.mjs
@@ -5,21 +5,6 @@ const nextConfig = BuilderDevTools()(
   BuilderDevTools()(
     BuilderDevTools()({
       reactStrictMode: true,
-      async redirects() {
-        return [
-          {
-            source: "/planner",
-            destination: "/planner/login",
-            permanent: true,
-          },
-          {
-            // this will match `/english(default)/something` being requested
-            source: "/mopita/register",
-            destination: "/api/mopita/register",
-            permanent: true,
-          },
-        ];
-      },
     })
   )
 );
--- src/components/site/footer/footercomponent.js
+++ src/components/site/footer/footercomponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Footer.module.css';
+import styles from './footer.module.css';
 import { footerLinks } from './footerlink';
 
 const Footer = () => {
