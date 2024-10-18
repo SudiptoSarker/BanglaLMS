d79dd3df4f52ebf3ed08f33f843686b8d8f12bc4
adnanjabait <adnan.jabait@gmail.com>
2024-10-18 12:23:30 +0600

    Modified in dev for production

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import LoginButton from "@/components/site/loginbutton/loginbuttoncomponent";
 import { useState,useEffect } from 'react';
 import { fetchSubscriptionLoginData,getSiteId,fetchNotificationsAndAnnouncements } from "@/components/api/queryApi";
 import TopPageComponent from "@/components/site/top/toppagecomponent";
+import { siteid } from "@/helper/helper";
 
 export default function HomePage({ globalData }) {
     // Demo URLs to pass as props
@@ -20,74 +21,75 @@ export default function HomePage({ globalData }) {
     const [subscriptionData, setSubscriptionData] = useState([]);
     const [loginData, setLoginData] = useState([]);
 
-    useEffect(() => {
-    // Get the domain name when the component mounts
-    if (typeof window !== "undefined") {
-        const currentDomain = window.location.hostname;
-        setDomain(currentDomain);
-        console.log("Current domain:", currentDomain); // For debugging
+    // useEffect(() => {
+    // // Get the domain name when the component mounts
+    // if (typeof window !== "undefined") {
+    //     const currentDomain = window.location.hostname;
+    //     setDomain(currentDomain);
+    //     console.log("Current domain:", currentDomain); // For debugging
+    // }
+    // }, []); // This useEffect runs only once, when the component mounts
+
+    const getSiteInformation = async () => {
+        try {
+            // const siteIdResult = await getSiteId(domain);
+            // if (!siteIdResult) {
+            //     throw new Error(`Site with name '${domain}' not found.`);
+            // }
+            
+            // const siteId = siteIdResult.data[0]?.id;
+            const domain = process.env.NEXT_PUBLIC_DOMAIN;
+            const siteInfo = await getSiteId(domain);
+            const siteId = siteInfo.data[0].id;
+            console.log("Extracted site ID:", siteId);
+            getSubscriptionData(siteId);       
+            getLoginData(siteId);     
+            getNotifications(siteId);
+            getAnnouncements(siteId);
+        } catch (error) {
+            console.log("Error fetching subscription data:", error);
+        }
+    };
+    
+    const getSubscriptionData = async (siteId) => {
+    try {            
+        const response = await fetchSubscriptionLoginData(siteId,"DeviceSubscriptionButton");
+        setSubscriptionData(response.data);
+    } catch (error) {
+        console.log("Error fetching subscription data:", error);
     }
-    }, []); // This useEffect runs only once, when the component mounts
+    };
+    const getLoginData = async (siteId) => {
+        try {            
+            const response = await fetchSubscriptionLoginData(siteId,"loginbutton");
+            setLoginData(response.data);
+        } catch (error) {
+            console.log("Error fetching subscription data:", error);
+        }
+    };
+
+    const getNotifications = async (siteId) => {
+        try {
+          const data = await fetchNotificationsAndAnnouncements(siteId,"notificationbanner");                  
+          setNotifications(data.data);
+        } catch (error) {
+          console.error("Error fetching notifications:", error);
+        }
+      };
+
+      const getAnnouncements = async (siteId) => {
+        try{
+          const data = await fetchNotificationsAndAnnouncements(siteId,"announcebanner");                  
+          setAnnouncements(data.data);
+        }catch(error) {
+          console.error("Error fetching announcements:", error);
+        }
+      };
 
     useEffect(() => {
         // Fetch subscription data once the domain is set
-        if (domain) {
-            const getSiteInformation = async () => {
-                try {
-                    const siteIdResult = await getSiteId(domain);
-                    if (!siteIdResult) {
-                        throw new Error(`Site with name '${domain}' not found.`);
-                    }
-                    
-                    const siteId = siteIdResult.data[0]?.id;
-                    console.log("Extracted site ID:", siteId);
-                    getSubscriptionData(siteId);       
-                    getLoginData(siteId);     
-                    getNotifications(siteId);
-                    getAnnouncements(siteId);
-                } catch (error) {
-                    console.log("Error fetching subscription data:", error);
-                }
-            };
-            
-            const getSubscriptionData = async (siteId) => {
-            try {            
-                const response = await fetchSubscriptionLoginData(siteId,"DeviceSubscriptionButton");
-                setSubscriptionData(response.data);
-            } catch (error) {
-                console.log("Error fetching subscription data:", error);
-            }
-            };
-            const getLoginData = async (siteId) => {
-                try {            
-                    const response = await fetchSubscriptionLoginData(siteId,"loginbutton");
-                    setLoginData(response.data);
-                } catch (error) {
-                    console.log("Error fetching subscription data:", error);
-                }
-            };
-
-            const getNotifications = async (siteId) => {
-                try {
-                  const data = await fetchNotificationsAndAnnouncements(siteId,"notificationbanner");                  
-                  setNotifications(data.data);
-                } catch (error) {
-                  console.error("Error fetching notifications:", error);
-                }
-              };
-
-              const getAnnouncements = async (siteId) => {
-                try{
-                  const data = await fetchNotificationsAndAnnouncements(siteId,"announcebanner");                  
-                  setAnnouncements(data.data);
-                }catch(error) {
-                  console.error("Error fetching announcements:", error);
-                }
-              };
-
-            getSiteInformation();
-        }
-    }, [domain]);  
+        getSiteInformation();
+    }, []);  
 
       
     return (
