eeb02808d5687797529ed9ace544324ba7f3dbc2
Adnan Ahsan <132339523+adnanjabait@users.noreply.github.com>
2024-11-17 22:30:04 +0600

    Modify middleware code

--- src/middleware.js
+++ src/middleware.js
@@ -2,11 +2,13 @@ import { NextResponse } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
+    console.log('middleware console:');
+    console.log(typeof(request));
     console.log(request);
   return NextResponse.next();
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/:path*',
+  matcher: ['/', '/top', '/member', '/unsubscribe', '/unsubscribed'],
 }
