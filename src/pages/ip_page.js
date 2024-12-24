d21e77d7ec2c92de558e4f647ce06e2b3ac8a0dd
Chandra Shekhar Das <ch.shekhardas@gmail.com>
2024-12-24 10:26:36 +0600

    add serversideprops

--- src/pages/ip_page.js
+++ src/pages/ip_page.js
@@ -1,13 +1,26 @@
 // Main layout component wrapping the page structure.
 import Layout from "@/components/site/layout/layout";
 
+export async function getServerSideProps(context) {
+    //const forwarded = req.headers["x-forwarded-for"]
+    //const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
+    
+    console.log('server context:\n',context)
+    return {
+      props: {
+        context:'done'
+      },
+    }
+  }
 
-export default function UnsubscribedPage() {
+export default function UnsubscribedPage({context}) {
 
     const handleClick = async ()=>{
         let data = await fetch('/api/it_test')
         let ip = await data.json()
         console.log(ip);
+
+        console.log(context)
     };
 
 
