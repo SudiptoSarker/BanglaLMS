56a8ec6c0bd17160af44a28cb14fd2c6ef59ae65
adnanjabait <adnan.jabait@gmail.com>
2025-02-26 10:00:35 +0600

    Modify test

--- src/pages/test.js
+++ src/pages/test.js
@@ -2,15 +2,20 @@ import Layout from "@/components/site/layout/layout";
 
 export async function getServerSideProps(context) {
 
+    const {query} = context;
+
+    const orderid = query.orderid || '0iUNJX%2Bdz%2Bfpf3GbYXoDdG8Acexp%2BQlL1uAQ%2BXVx2oY%3D';
+
         const siteMode = '0';
         return {
             props: {
-                siteMode: siteMode
+                siteMode: siteMode,
+                order: orderid
             }
         }
     }
 
-export default function TestPage({siteMode}) {
+export default function TestPage({siteMode, order}) {
     const handlePaylist = async (serviceID) => {
         const response = await fetch(`/api/mopita/paylist?siteMode=${siteMode}&service=${serviceID}`)
         const result = await response.json();
@@ -31,10 +36,13 @@ export default function TestPage({siteMode}) {
 
     return (
         // Main layout wrapping the page structure.
-        <Layout>                 
-            <button onClick={() => handlePaylist('R000002750')}>Paylist</button>
-            <button onClick={() => handleBeforePay('R000002750', '00')}>Before Pay</button>         
-            <button onClick={() => handleAfterPay('R000002750', '00', '0iUNJX%2Bdz%2Bfpf3GbYXoDdG8Acexp%2BQlL1uAQ%2BXVx2oY%3D')}>After Pay</button>         
+        <Layout>
+            <input type="text" name="serviceid" id="serviceid" />                 
+            <input type="text" name="paytype" id="paytype"/>                 
+            <button onClick={() => handlePaylist(document.getElementById('serviceid').value)}>Paylist</button>
+            <button onClick={() => handleBeforePay(document.getElementById('serviceid').value, document.getElementById('paytype').value)}>Before Pay</button>         
+            <button onClick={() => handleAfterPay(document.getElementById('serviceid').value, document.getElementById('paytype').value, order )}>After Pay</button>         
+
                       
         </Layout>
     );
