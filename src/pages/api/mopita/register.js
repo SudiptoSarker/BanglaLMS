c0237c81be74b08bb22bc2ae2d272dad92dc452b
Chandra Shekhar Das <ch.shekhardas@gmail.com>
2025-03-04 18:36:06 +0600

    added code.

--- src/pages/api/mopita/register.js
+++ src/pages/api/mopita/register.js
@@ -2,7 +2,7 @@ import { queryDatabase } from '@/lib/config';
 import fs from 'fs'
 import path from 'path'
 import { siteid,isNullOrEmpty } from '@/helper/helper';
-import { getMemberList,insertMember,createUserLog,getCI } from '@/components/api/queryApi';
+import { getMemberList,insertMember,createUserLog,getCI,getMemberListByOrderId } from '@/components/api/queryApi';
 
 export default async function handler(req, res) {
     {
@@ -63,73 +63,89 @@ export default async function handler(req, res) {
         res.status(200).send('NG¥n');
     }
 
-    try{
-        // dev
-        //let jsonBody = {"uid":"279d0664343d1bba04","ci":"R000002750","act":"reg","cs":"20241001000000000","iai_tms":"20240904192455905","iai_paytype":"00","iai_ordid":"202409046fc1693bf60e81e074","arg":""};
-        // let jsonBody = JSON.parse(req.body);
-    
-        
-        let siteId = await siteid();
+     // check member by order id
+     let memberListByOrderId = await getMemberListByOrderId(siteId,ci,uid,orderId);
 
-        // check member
-        let memberList = await getMemberList(siteId,ci,uid);
-
-        if(memberList.data.length > 0){
-            isMember = true;
+    if(memberListByOrderId.data.length > 0){
+        let firstMember = memberListByOrderId[0];
+        if(firstMember.licensekey != null && firstMember.licensekey.length > 0){
+            res.status(200).send('OK¥n');
         }
         else{
-
-            let _ci = await getCI(siteId,ci);
-            if(_ci.data.length > 0){
-
-                let memberObject = {
-                    siteId:siteId,
-                    ci:ci,
-                    uid:uid,
-                    orderId:orderId,
-                    orderTime:orderTime,
-                    payType:payType,
-                    isMember:1,
-                    cs:cs,
-                    category:_ci.data[0].category
-                };
-
-                let createMember = await insertMember(memberObject);
-
-                if(createMember.data[0].newId > 0){
-                    isMember = true;
-                    activity = 'subscriptions';
-                }
-            }
-
+            res.status(200).send('NG¥n');
         }
-        
-    }
-    catch(error){
-        res.status(200).send('NG¥n');
-    }finally{
-        try{
-            // creating user log.
-            let userLog  = {
-                uid:uid,
-                pageLink:'',
-                activity:activity,
-                time:new Date().toISOString().replace('T', ' ').substring(0, 19) 
-            };
-
-            let response = await createUserLog(userLog);
-        }
-        catch(error){
-        }
-        
-    }
-
-    if(isMember){
-        res.status(200).send('OK¥n');
     }
     else{
-        res.status(200).send('NG¥n');
+        try{
+            // dev
+            //let jsonBody = {"uid":"279d0664343d1bba04","ci":"R000002750","act":"reg","cs":"20241001000000000","iai_tms":"20240904192455905","iai_paytype":"00","iai_ordid":"202409046fc1693bf60e81e074","arg":""};
+            // let jsonBody = JSON.parse(req.body);
+        
+            
+            let siteId = await siteid();
+    
+            // check member
+            let memberList = await getMemberList(siteId,ci,uid);
+    
+            if(memberList.data.length > 0){
+                isMember = true;
+            }
+            else{
+    
+                let _ci = await getCI(siteId,ci);
+                if(_ci.data.length > 0){
+    
+                    let memberObject = {
+                        siteId:siteId,
+                        ci:ci,
+                        uid:uid,
+                        orderId:orderId,
+                        orderTime:orderTime,
+                        payType:payType,
+                        isMember:1,
+                        cs:cs,
+                        category:_ci.data[0].category
+                    };
+    
+                    let createMember = await insertMember(memberObject);
+    
+                    if(createMember.data[0].newId > 0){
+                        isMember = true;
+                        activity = 'subscriptions';
+                    }
+                }
+    
+            }
+            
+        }
+        catch(error){
+            res.status(200).send('NG¥n');
+        }finally{
+            try{
+                // creating user log.
+                let userLog  = {
+                    uid:uid,
+                    pageLink:'',
+                    activity:activity,
+                    time:new Date().toISOString().replace('T', ' ').substring(0, 19) 
+                };
+    
+                let response = await createUserLog(userLog);
+            }
+            catch(error){
+            }
+            
+        }
+    
+        if(isMember){
+            res.status(200).send('OK¥n');
+        }
+        else{
+            res.status(200).send('NG¥n');
+        }
     }
+
+  
 
     
 }
